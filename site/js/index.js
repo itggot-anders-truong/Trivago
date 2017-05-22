15779fe0a07321b2abfd4b17e220e3e368dd921d
itggot-anders-truong <anders.truong@itggot.se>
2017-05-22 09:56:44 +0200

    Klar
    Optimerat margins på olika objekter, skapat funktion åt slideshow på
    bilderna
    Klar med desktopversion!

--- site/js/index.js
+++ site/js/index.js
@@ -15,3 +15,22 @@ $('.navhotell').click(function (e) {
         scrollTop: $('#hotell').offset().top - 125
     }, 'slow');
 });
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("hide");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.opacity = "0";
+       x[i].style.transition = "1s";  
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block"; 
+    x[myIndex-1].style.opacity = "1";  
+    x[myIndex-1].style.transition = "1s";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
